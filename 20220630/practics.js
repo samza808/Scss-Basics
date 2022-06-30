a3182bd0151df1677b9932784e8cadee4482de4e
kwonSuYoun <balenti808@gmail.com>
2022-06-30 15:20:19 +0900

    20220630

---
+++ 20220630/practics.js
@@ -0,0 +1,21 @@
+// function one(ipgoo) {
+
+//   if(ipgoo === '바보') {
+//     return '나 바보아닌데';
+//   } else {
+//     return "천재";
+//   }
+
+// }
+
+// console.log(one("바보"));
+
+// -------------------------
+
+function one (ipgoo, ipgootwo) {
+
+  return `${ipgoo} ${''} ${ipgootwo}`;
+
+}
+
+console.log(one('아버지가', '방에 들어가신다'));
